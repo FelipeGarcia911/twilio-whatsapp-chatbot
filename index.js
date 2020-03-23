e026de5f069ad89cd3005c86de4a2f06e5fb67aa
Felipe García <arfgarciama@unal.edu.co>
2020-03-23 18:59:13 +0000

    Add DialogFlow

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+// General imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
+const uuid = require('uuid');
 
 // Init Twillio API
 const accountSid = process.env.TWILLIO_SID;
 const authToken = process.env.TWILLIO_TOKEN;
 const client = new twilio(accountSid, authToken);
 const MessagingResponse = twilio.twiml.MessagingResponse;
+const twiml = new MessagingResponse();
 
+// Init DialogFlow
+const DialogFlowClass = require('./DialogFlowClass');
+const dialogFlowClass = new DialogFlowClass();
+
+// Init Express
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/incoming', (req, res) => {
-  const twiml = new MessagingResponse();
-  console.log(req.body)
+app.post('/incoming', async (req, res) => {
+  const sessionId = uuid.v4();
+  const { Body = '', From = '' } = req.body;
+  
+  const response = await dialogFlowClass.sendTextMessageToDialogFlow(Body, sessionId);
 });
 
 app.post('/status', (req, res) => {
